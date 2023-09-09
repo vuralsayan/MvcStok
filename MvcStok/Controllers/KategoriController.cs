935d1fef1f2d43f25801d61ed9f39fbe7a31e463
Vural Sayan <vuralsayan01@gmail.com>
2023-09-09 22:55:57 +0300

    Kategoriler listelendi

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcStok.Models.Entity;
 
 namespace MvcStok.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities(); 
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORILER.ToList();    
+            return View(degerler);
         }
     }
 }
