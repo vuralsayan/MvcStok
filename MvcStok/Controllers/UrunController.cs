fbf4b2b4ec92d2e7dfcb61e1d3136688f0d532aa
Vural Sayan <vuralsayan01@gmail.com>
2023-09-09 23:28:57 +0300

    Ürünler listelendi

---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity; 
+
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities(); 
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();  
+            return View(degerler);
+        }
+    }
+}
