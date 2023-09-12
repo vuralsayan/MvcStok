322d262f5a1d40b5ca4eae236236f91dea8fef0a
Vural Sayan <vuralsayan01@gmail.com>
2023-09-12 21:17:33 +0300

    Validation Kontrolleri - Required

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -26,6 +26,11 @@ namespace MvcStok.Controllers
         [HttpPost] 
         public ActionResult YeniKategori(TBLKATEGORILER p1)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+
             db.TBLKATEGORILER.Add(p1);
             db.SaveChanges();
             return View();
